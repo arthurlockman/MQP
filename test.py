75f0875451b6503bbe2eeba223a3584fe3e3cb5e
TuckerHaydon <tuckerhaydon@gmail.com>
2015-11-15 22:23:18 -0500

    This code. It very confusing.

--- test.py
+++ test.py
@@ -26,12 +26,67 @@ class ImageProcessor(threading.Thread):
                     self.stream.seek(0)
                     # Read the image and do some processing on it
                     img = Image.open(self.stream)
-                    #...
-                    #...
-                    # Set done to True if you want the script to terminate
-                    # at some point
+                    frame = img
+
+                    # convert the frame to grayscale, blur it, and detect edges
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    blur = cv2.GaussianBlur(gray, (7, 7), 0)
+                    edge = cv2.Canny(blur, 50, 150)
+
+                    # find contours in the edge map
+                    (_, cnts, _) = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                    # loop over the contours
+                    for c in cnts:
+                    # approximate the contour
+                    peri = cv2.arcLength(c, True)
+                    approx = cv2.approxPolyDP(c, 0.01 * peri, True)
+
+                    # ensure that the approximated contour is "roughly" rectangular
+                    if len(approx) >= 4 and len(approx) <= 7:
+                        # compute the bounding box of the approximated contour and
+                        # use the bounding box to compute the aspect ratio
+                        (x, y, w, h) = cv2.boundingRect(approx)
+                        aspectRatio = w / float(h)
+
+                        # compute the solidity of the original contour
+                        area = cv2.contourArea(c)
+                        hullArea = cv2.contourArea(cv2.convexHull(c))
+                        solidity = area / float(hullArea)
+
+                        # compute whether or not the width and height, solidity, and
+                        # aspect ratio of the contour falls within appropriate bounds
+                        keepDims = w > 10 and h > 10
+                        keepSolidity = solidity > 0.9
+                            # keepSolidity = True
+                        keepAspectRatio = aspectRatio >= 0.8 and aspectRatio <= 1.2
+
+                        # ensure that the contour passes all our tests
+                        if keepDims and keepSolidity and keepAspectRatio:
+                            # draw an outline around the target and update the status
+                            # text
+                            cv2.drawContours(frame, [approx], -1, (255, 0, 0), 4)
+
+                                try:
+                                    # compute the center of the contour region and draw the
+                                    # crosshairs
+                                    M = cv2.moments(approx)
+                                    (cX, cY) = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                                    (startX, endX) = (int(cX - (w * 0.15)), int(cX + (w * 0.15)))
+                                    (startY, endY) = (int(cY - (h * 0.15)), int(cY + (h * 0.15)))
+                                    cv2.line(frame, (startX, cY), (endX, cY), (0, 0, 255), 3)
+                                    cv2.line(frame, (cX, startY), (cX, endY), (0, 0, 255), 3)
+                                except Exception:
+                                    print "Divide by zero"
+
+                    # show the frame and record if a key is pressed
+                    cv2.imshow("Frame", frame)
+                    key = cv2.waitKey(1) & 0xFF
+
+                    # if the 'q' key is pressed, stop the loop
+                    if key == ord("q"):
+                    break
                     # done=True
-                    current_time = int(round(time.time()*1000))
                 finally:
                     # Reset the stream and event
                     self.stream.seek(0)
